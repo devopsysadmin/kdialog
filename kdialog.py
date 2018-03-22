58f08b5bb85e6b25bf9464783fa23b20b024d518
David Pedersen <josedavid.pedersen.contractor@bbva.com>
2018-03-22 11:10:25 +0100

    Implementation of radiolist

--- kdialog.py
+++ kdialog.py
@@ -1,13 +1,13 @@
 import subprocess
 
-def __get_extras(**kwargs):
-    extras = list()
-    def put_in_extras(key):
+def __init(**kwargs):
+    cmd = ['kdialog']
+    def append_optionals(key):
         if kwargs.get(key, None) is not None:
-            extras.append(key)
-            extras.append(kwargs[key])
-
-    put_in_extras('geometry')
+            cmd.append(key)
+            cmd.append(kwargs[key])
+    append_optionals('geometry')
+    return cmd
 
 
 def __run(cmd):
@@ -18,7 +18,21 @@ def __run(cmd):
 
 
 def msgbox(text, **kwargs):
-    cmd =['kdialog', '--msgbox', text]
-    if kwargs:
-         cmd += __get_extras(**kwargs)
+    cmd = __init(**kwargs) + ['--msgbox', text]
+    return __run(cmd)
+
+'''
+elements is an array which contains a list of tuples of 3 strings:
+- return value: a string without spaces or special characters
+- title: how will be shown in display
+- state: 'on' or 'off'
+Note: If there are more than one item in 'on', the last one will be taken as 'on'
+Ex: [('foo', 'Something nice', 'off'), ('bar', 'Something ugly', 'off')]
+'''
+def radiolist(caption, elements, **kwargs):
+    cmd = __init(**kwargs) + ['--radiolist', caption ]
+    for key, title, state in [ element for element in elements ]:
+        cmd.append('%s' %key)
+        cmd.append(u'%s' %title)
+        cmd.append('%s' %state)
     return __run(cmd)
