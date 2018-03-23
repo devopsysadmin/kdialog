7925b5a6d2a8c5907db932853050a8523dc3ff0f
David Pedersen <josedavid.pedersen.contractor@bbva.com>
2018-03-23 10:56:20 +0100

    Implemented detailed_error and kdesu (though this one doesn't really belong to kdialog, this may be really useful)

--- kdialog.py
+++ kdialog.py
@@ -1,4 +1,5 @@
 import subprocess
+from shlex import split
 
 def __init(**kwargs):
     cmd = ['kdialog']
@@ -12,9 +13,7 @@ def __init(**kwargs):
 
 def __run(cmd):
     proc = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    rc = proc.returncode
-    stdout = proc.stdout.decode('utf8')
-    return rc, stdout
+    return proc.returncode, proc.stdout.decode('utf8')
 
 
 def msgbox(text, **kwargs):
@@ -22,17 +21,25 @@ def msgbox(text, **kwargs):
     return __run(cmd)
 
 '''
-elements is an array which contains a list of tuples of 3 strings:
-- return value: a string without spaces or special characters
-- title: how will be shown in display
-- state: 'on' or 'off'
-Note: If there are more than one item in 'on', the last one will be taken as 'on'
-Ex: [('foo', 'Something nice', 'off'), ('bar', 'Something ugly', 'off')]
+*elements* is list of tuples. Each tuple contains 3 values:
+- (str) return value: a string without spaces or special characters
+- (str) title: how will be shown in display
+- (bool) checked: Determine if this item is checked when radiolist appears
+Note: If there are more than one item checked, the last one applies
+Ex: [('foo', 'Something nice', False), ('bar', 'Something ugly', True)]
 '''
 def radiolist(caption, elements, **kwargs):
     cmd = __init(**kwargs) + ['--radiolist', caption ]
-    for key, title, state in [ element for element in elements ]:
+    for key, title, enabled in [ element for element in elements ]:
         cmd.append('%s' %key)
         cmd.append(u'%s' %title)
-        cmd.append('%s' %state)
+        cmd.append('on' if enabled else 'off')
     return __run(cmd)
+
+
+def detailed_error(caption, details, **kwargs):
+    cmd = __init(**kwargs) + [ '--detailederror', caption, details ]
+    return __run(cmd)
+
+def kdesu(command):
+    return __run(shlex.split('kdesu -t -c %s' %command))
